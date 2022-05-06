87c50a71d9c286dcd97b8a25a241f8c82707d41c
Gia Huy <nguyengiahuy16@gmail.com>
2022-05-06 16:18:03 +0700

    setup pj

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export function a(a: number, b: number) {
+    return a + b + 0;
+}
+
+console.log(a(1, 2));
---
+++ test/test.spec.ts
@@ -0,0 +1,7 @@
+import {a} from "../src";
+
+describe('test', () => {
+    it('test', () => {
+        expect(a(1,2)).toEqual(3);
+    })
+})
