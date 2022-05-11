4ae24c55e879a0b53947100b2298c8239f1c0a08
Gia Huy <nguyengiahuy16@gmail.com>
2022-05-11 14:04:34 +0700

    add ca gen

--- src/ca.ts
+++ src/ca.ts
@@ -1,6 +1,7 @@
 import * as forge from 'node-forge';
 import * as fs from "fs";
 import { v4 as uuid } from "uuid";
+import * as buffer from "buffer";
 
 /**
  *  refer
@@ -99,7 +100,7 @@ export class PPCa {
 
         cert.setSubject([
             { name: 'commonName', value: domain },
-            { name: 'organizationName', value: 'Mockttp Cert - DO NOT TRUST' }
+            { name: 'organizationName', value: 'PMS CA' }
         ]);
         cert.setIssuer(this.caCert.subject.attributes);
 
@@ -128,5 +129,72 @@ export class PPCa {
 
         this.certCache[domain] = generatedCertificate;
         return generatedCertificate;
+    }
+
+    /**
+     * Generate a CA certificate for mocking HTTPS.
+     *
+     * Returns a promise, for an object with key and cert properties,
+     * containing the generated private key and certificate in PEM format.
+     *
+     * These can be saved to disk, and their paths passed
+     * as HTTPS options to a Mockttp server.
+     */
+     static async generateCACertificate(options: { commonName?: string, bits?: number } = {}) {
+        if (!options.commonName) {
+            options.commonName = 'PMS CA';
+        }
+
+        if (!options.bits) {
+            options.bits = 2048;
+        }
+
+        const keyPair = await new Promise<forge.pki.rsa.KeyPair>((resolve, reject) => {
+            forge.pki.rsa.generateKeyPair({ bits: options.bits }, (error, keyPair) => {
+                if (error) reject(error);
+                else resolve(keyPair);
+            });
+        });
+
+        const cert = forge.pki.createCertificate();
+        cert.publicKey = keyPair.publicKey;
+        cert.serialNumber = uuid().replace(/-/g, '');
+
+        cert.validity.notBefore = new Date();
+        // Make it valid for the last 24h - helps in cases where clocks slightly disagree
+        cert.validity.notBefore.setDate(cert.validity.notBefore.getDate() - 1);
+
+        cert.validity.notAfter = new Date();
+        // Valid for the next year by default.
+        cert.validity.notAfter.setFullYear(cert.validity.notAfter.getFullYear() + 1);
+
+        cert.setSubject([{ name: 'commonName', value: options.commonName }]);
+
+        cert.setExtensions([{
+            name: 'basicConstraints',
+            cA: true
+        }]);
+
+        // Self-issued too
+        cert.setIssuer(cert.subject.attributes);
+
+        // Self-sign the certificate - we're the root
+        cert.sign(keyPair.privateKey, forge.md.sha256.create());
+
+        return {
+            key: forge.pki.privateKeyToPem(keyPair.privateKey),
+            cert: forge.pki.certificateToPem(cert)
+        };
+    }
+
+    static generateSPKIFingerprint(certPem: string | Buffer) {
+        let cert = forge.pki.certificateFromPem(certPem.toString('utf8'));
+        return forge.util.encode64(
+            forge.pki.getPublicKeyFingerprint(cert.publicKey, {
+                type: 'SubjectPublicKeyInfo',
+                md: forge.md.sha256.create(),
+                encoding: 'binary'
+            })
+        );
     }
 }
